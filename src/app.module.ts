e43990b0f34790cfffbde11a0aba8d9846df3f9d
Kamil Mastalerz <kamil.mastalerz@netguru.com>
2022-01-19 19:21:28 +0100

    Add movies graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/test')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
