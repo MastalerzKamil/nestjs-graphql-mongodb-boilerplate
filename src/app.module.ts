225132d7ec4244f1072801d30ba8481616dfb74d
Kamil Mastalerz <kamil.mastalerz@netguru.com>
2022-01-19 19:27:40 +0100

    Add usage of grqphql console

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    GraphQLModule.forRoot({
+      debug: false,
+      installSubscriptionHandlers: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
     MoviesModule,
   ],
   controllers: [AppController],
